3b5e7d3cd974db472ae5d4659fad56eb6ab08851
Shivam Shrestha <3rdsty4bl00d@gmail.com>
2018-11-16 12:28:48 +0545

    changed the app.vue

--- src/App.vue
+++ src/App.vue
@@ -21,9 +21,10 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
                     <h1>Custom directives</h1>
-                    <p v-highlight.delayed = "'red'">Color this</p>
-                    
-                
+                    <p v-highlight.delayed="'red'">Color this</p>
+                    <p v-local-highlight.delayed="'red'">Color this, too</p>
+
+
                 </div>
             </div>
         </div>
@@ -31,6 +32,38 @@
 </template>
 
 <script>
+    export default {
+        directives: {
+            localHighlight: {
+                bind(el, binding, vnode) {
+                    /*el.style.backgroundColor = binding.value;*/
+                    /*el.style.backgroundColor = 'green;*/
+                    var delay = 0;
+                    if (binding.modifiers['delayed']) {
+                        delay = 5000;
+                    }
+                    /*
+                        this is what we call 'awesome'
+                        setTimeout() is a javascript function
+                        above
+                        we checked if we have a modifier in the 
+                        custom made directive
+                        here, modifier is a string object
+                        if we have a modifier then the value of delay will set to 3sec
+                        then the color of background is changed 
+                        to what the user has given
+                    */
+                    setTimeout(() => {
+                        if (binding.arg == 'background') {
+                            el.style.backgroundColor = binding.value;
+                        } else {
+                            el.style.color = binding.value;
+                        }
+                    }, delay);
+                }
+            }
+        }
+    }
 
 </script>
 
