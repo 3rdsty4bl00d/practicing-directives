b9c1277531df0276dd8a4e191eb239647c65513d
Shivam Shrestha <3rdsty4bl00d@gmail.com>
2018-11-16 10:44:17 +0545

    added some built in directives

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+                    <h1>Built-in directives</h1>
+                    <p v-text="'v-text directive'"></p>
+                    <p v-html="'<strong>v-html directive</strong>'"></p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
